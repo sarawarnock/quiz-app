f1c31e37c8846f5898ab74ab86813708b821e0ca
sarawarnock <sara@sarawarnock.com>
2020-01-21 09:52:26 -0700

    img

--- datastore.js
+++ datastore.js
@@ -3,60 +3,80 @@ const STORE = [
     {
         question: 'How many common types of recycling are there?',
         answers: [5, 12, 8, 2],
-        correctAnswer: 8
+        correctAnswer: 8,
+        img: '',
+        alt: ''
     },
 
     {
         question: 'Which of these cannot be recycled in a regular curbside bin?',
         answers: ['Styrofoam and bubble wrap', 'Plasic grocery bags and paper towels','Batteries and electronics', 'All of the above'],
-        correctAnswer: 'All of the above'
+        correctAnswer: 'All of the above',
+        img: '',
+        alt: ''
     },
 
     {
         question: 'What are three things that are not considered traditional recycling, but can be recycled?',
         answers: ['Coffee cups and packing peanuts', 'Crayons, running shoes, and wine corks', 'Hardcover books, plastic shower curtains, and hangers', 'All of the above'],
-        correctAnswer: 'Crayons, running shoes, and wine corks'
+        correctAnswer: 'Crayons, running shoes, and wine corks',
+        img: '',
+        alt: ''
     },
 
     {
         question: 'How much trash does the average American throw away PER DAY (in lbs)?',
         answers: [7.6,  1.5, 4.4, 10],
-        correctAnswer: 4.4
+        correctAnswer: 4.4,
+        img: '',
+        alt: ''
     },
 
     {
         question: 'What percentage of that trash could instead be recycled?',
         answers: [100, 25, 50, 60],
-        correctAnswer: 60
+        correctAnswer: 60,
+        img: '',
+        alt: ''
     },
 
     {
         question: 'What is one of the biggest problems caused by landfills?',
         answers: ['They take up too much space', 'They are ugly', 'They leak toxic substances into the soil and water', 'They start fires'],
-        correctAnswer: 'They leak toxic substances into the soil and water'
+        correctAnswer: 'They leak toxic substances into the soil and water',
+        img: '',
+        alt: ''
     },
 
     {
         question: 'Which recyclable items are commonly forgotton about?',
         answers: ['Beer bottles and cans', 'Plastic water bottles', 'Cereal boxex', 'Shampoo and conditioner bottles'],
-        correctAnswer: 'Shampoo and conditioner bottles'
+        correctAnswer: 'Shampoo and conditioner bottles',
+        img: '',
+        alt: ''
     },
 
     {
         question: 'How many plastic bottles are thrown away EACH HOUR in the United States?',
         answers: ['2.5 million', '10 million', '100,000',  '50,000'],
-        correctAnswer: '2.5 million'
+        correctAnswer: '2.5 million',
+        img: '',
+        alt: ''
     },
 
     {
         question: 'How long does it take for an average size plastic bottle to decompose?',
         answers: ['100 years', '450 years', '10 years', '250 years'],
-        correctAnswer: '450 years'
+        correctAnswer: '450 years',
+        img: '',
+        alt: ''
     },
 
     {
         question: 'What is the biggest mistake people make when recycling?',
         answers: ['Recycling dirty food containers', 'Bagging up all recyclables like trash', 'Recycling materials with a plastic or wax coating', 'Not checking with their local recycing program'],
-        correctAnswer: 'Not checking with their local recycling program'
+        correctAnswer: 'Not checking with their local recycling program',
+        img: '',
+        alt: ''
     }
 ];

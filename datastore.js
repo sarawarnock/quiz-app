baad7bfad00c7973a964c0619e35a72e5321950a
sarawarnock <sara@sarawarnock.com>
2020-01-27 21:49:23 -0700

    added images

--- datastore.js
+++ datastore.js
@@ -4,7 +4,7 @@ const STORE = [
         question: 'How many common types of recycling are there?',
         answers: [5, 12, 8, 2],
         correctAnswer: 8,
-        img: 'https://i.imgur.com/eaIKGD1.png',
+        img: 'images/watercolor8.png',
         alt: 'image of the number 8'
     },
 
@@ -12,7 +12,7 @@ const STORE = [
         question: 'Which of these cannot be recycled in a regular curbside bin?',
         answers: ['Styrofoam and bubble wrap', 'Plasic grocery bags and paper towels','Batteries and electronics', 'All of the above'],
         correctAnswer: 'All of the above',
-        img: 'https://i.imgur.com/Sz8Yape.jpg',
+        img: 'images/plasticbag.jpg',
         alt: 'sea creatures in plastic bag'
     },
 
@@ -20,7 +20,7 @@ const STORE = [
         question: 'Which of these are not considered traditional recycling, but can be recycled in special locations?',
         answers: ['Coffee cups and packing peanuts', 'Crayons, running shoes, and wine corks', 'Hardcover books, plastic shower curtains, and hangers', 'All of the above'],
         correctAnswer: 'Crayons, running shoes, and wine corks',
-        img: 'https://i.imgur.com/k0V3ShU.jpg',
+        img: 'images/runningshoes (1).jpg',
         alt: 'running shoes icon'
     },
 
@@ -28,7 +28,7 @@ const STORE = [
         question: 'How much trash does the average American throw away PER DAY (in lbs)?',
         answers: [7.6,  1.5, 4.4, 10],
         correctAnswer: 4.4,
-        img: 'https://i.imgur.com/15xIFCB.jpg',
+        img: 'images/trashcans (1).jpg',
         alt: 'three trash bins'
     },
 
@@ -36,7 +36,7 @@ const STORE = [
         question: 'What percentage of that trash could instead be recycled?',
         answers: [100, 25, 50, 60],
         correctAnswer: "60",
-        img: 'https://i.imgur.com/wAuCnpO.jpg',
+        img: 'images/60.jpg',
         alt: '60% image'
     },
 
@@ -44,7 +44,7 @@ const STORE = [
         question: 'What is one of the biggest problems caused by landfills?',
         answers: ['They take up too much space', 'They are ugly', 'They leak toxic substances into the soil and water', 'They start fires'],
         correctAnswer: 'They leak toxic substances into the soil and water',
-        img: 'https://i.imgur.com/NwzaT5i.jpg',
+        img: 'images/lanfill.jpeg',
         alt: 'trash sitting in a landfill'
     },
 
@@ -52,7 +52,7 @@ const STORE = [
         question: 'Which recyclable items are commonly forgotton about?',
         answers: ['Beer bottles and cans', 'Plastic water bottles', 'Cereal boxes', 'Shampoo and conditioner bottles'],
         correctAnswer: 'Shampoo and conditioner bottles',
-        img: 'https://i.imgur.com/uaATbw4.jpg',
+        img: 'images/cosmetics.jpg',
         alt: 'shampoo and conditioner bottles'
     },
 
@@ -60,7 +60,7 @@ const STORE = [
         question: 'How many plastic bottles are thrown away EACH HOUR in the United States?',
         answers: ['2.5 million', '10 million', '100,000',  '50,000'],
         correctAnswer: '2.5 million',
-        img: 'https://i.imgur.com/TbLHwE4.jpg',
+        img: 'images/plasticbottles.jpg',
         alt: 'various plastic bottles'
     },
 
@@ -68,7 +68,7 @@ const STORE = [
         question: 'How long does it take for an average size plastic bottle to decompose?',
         answers: ['100 years', '450 years', '10 years', '250 years'],
         correctAnswer: '450 years',
-        img: 'https://i.imgur.com/Zu2RMTL.jpg',
+        img: 'images/sodabottles (1).jpg',
         alt: 'crushed soda bottles'
     },
 
@@ -76,7 +76,7 @@ const STORE = [
         question: 'What is the biggest mistake people make when recycling?',
         answers: ['Recycling dirty food containers', 'Bagging up all recyclables like trash', 'Recycling materials with a plastic or wax coating', 'Not checking with their local recycling program'],
         correctAnswer: 'Not checking with their local recycling program',
-        img: 'https://i.imgur.com/axfOqUx.jpg',
+        img: 'images/earthwatercolor (1).jpg',
         alt: 'earth icon'
     }
 ];
